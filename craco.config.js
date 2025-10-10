14205a6d74a3776528b69cd7dfc8a289b7097e4a
노다현 <dazi@nodahyeon-ui-MacBookPro.local>
2025-10-11 02:02:05 +0900

    error check

---
+++ craco.config.js
@@ -0,0 +1,8 @@
+// craco.config.js
+module.exports = {
+  style: {
+    postcss: {
+      mode: "file", // 루트의 postcss.config.js 사용하도록 강제
+    },
+  },
+};
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {}, // tailwindcss 대신 @tailwindcss/postcss 사용
-    autoprefixer: {},
-  }
-}
+module.exports = {
+  plugins: ["@tailwindcss/postcss"],
+};
